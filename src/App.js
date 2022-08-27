20574cd0fe237e52d3f4f3174b9d79fd1798b118
Jaroslaw Charchula <charchula@gmail.com>
2022-08-27 03:17:26 +0200

    create file structure create Header component create header css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <div className='App'>im alive</div>;
+  return (
+    <div className='App'>
+      <Header />
+
+      {/* home */}
+    </div>
+  );
 }
 
 export default App;
