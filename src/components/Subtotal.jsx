e95bb1c0b5cf443f94cf927a6c2a0dbedaf62227
Jaroslaw Charchula <charchula@gmail.com>
2022-08-30 17:41:19 +0200

    edit Subtotal components edit subtotal css add fixes to css header

--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import '../styles/subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 
 const Subtotal = () => {
-  return <div className='subtotal'>Subtotal</div>;
+  return (
+    <div className='subtotal'>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* home work */}
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small className='subtotal__gift'>
+              <input type='checkbox' /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0} // home work
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 };
 
 export default Subtotal;
