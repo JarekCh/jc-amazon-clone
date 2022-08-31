994ec17ea4404e5aa858c9ebfd378be3b3074850
Jaroslaw Charchula <charchula@gmail.com>
2022-08-31 20:37:29 +0200

    add error and login css file refactor app route edit error and login page add link to login page in header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Checkout from './pages/Checkout';
+import Login from './pages/Login';
+import SharedLayout from './pages/SharedLayout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Error from './pages/Error';
 
@@ -9,11 +11,13 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='*' element={<Error />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/' element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='*' element={<Error />} />
+          </Route>
         </Routes>
       </div>
     </Router>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/error.css';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
