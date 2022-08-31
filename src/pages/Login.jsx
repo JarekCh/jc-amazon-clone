88c8b90d5d25349c9126fae95b609c65a0e96e0b
Jaroslaw Charchula <charchula@gmail.com>
2022-09-01 00:20:54 +0200

    edit Login page add jsx edit partial login css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/login.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='login'>
+      <Link to='/'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+          className='login__logo'
+        />
+      </Link>
+      <div className='login__container'>
+        <h1>Sing-In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type='email' />
+
+          <h5>Password</h5>
+          <input type='password' />
+
+          <button className='login__singInButton'>Sing In</button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button className='login__registerButton'>
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
