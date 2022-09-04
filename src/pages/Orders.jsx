59e6e79cc79eef28d8e31653d2e27b951f641a72
Jaroslaw Charchula <charchula@gmail.com>
2022-09-04 23:38:16 +0200

    add acces to db collection oreders in firebase

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/orders.css';
+import { db } from '../firebase';
+import { useGlobalContext } from '../StateProvider';
 
 const Orders = () => {
+  const [{ cart, user }, dispatch] = useGlobalContext();
+  const [orders, setOrders] = useState([]);
+  console.log('🚀 ~ file: Orders.jsx ~ line 9 ~ Orders ~ setOrders', orders);
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users')
+        .doc(user?.uid)
+        .collection('orders')
+        .orderBy('created', 'desc')
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
+
+      <div className='orders__order'></div>
     </div>
   );
 };
